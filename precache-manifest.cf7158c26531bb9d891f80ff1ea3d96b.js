3d6fcf3df785227c8da98917a54be41b963ee7f1
ArtemiiKab <artemiikab@gmail.com>
2020-09-28 12:58:19 +0300

    Updates

---
+++ precache-manifest.cf7158c26531bb9d891f80ff1ea3d96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2fe7bada23be49bfef473acbedb6fe",
+    "url": "/WeRdevs/index.html"
+  },
+  {
+    "revision": "5f2a9539cd185292cf71",
+    "url": "/WeRdevs/static/css/main.a6dc84cd.chunk.css"
+  },
+  {
+    "revision": "4d8570a85e21446b6b9a",
+    "url": "/WeRdevs/static/js/2.7abbd222.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WeRdevs/static/js/2.7abbd222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2a9539cd185292cf71",
+    "url": "/WeRdevs/static/js/main.cc106b64.chunk.js"
+  },
+  {
+    "revision": "431a5651dfe4aa14163b",
+    "url": "/WeRdevs/static/js/runtime-main.639125ca.js"
+  }
+]);
